0f795686857504762ad139a522dca4ce3a5df3e9
awarnes <alex.warnes@moovel.com>
2023-02-24 10:59:54 -0800

    Add some parse testing, start on integration test harness

---
+++ test/generate.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const {faker} = require('@faker-js/faker');
+
+/**
+ * Generate destination data for testing
+ */
+function destinations() {
+
+}
+
+/**
+ * Generate driver data for testing
+ */
+function drivers() {
+
+}
+
+module.exports = {
+  destinations,
+  drivers
+}
---
+++ test/integration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO: Maybe put this all in a JS test instead
+
+# Generate destination data
+
+# Generate driver data
+
+# Test
